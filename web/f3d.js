e81cdbcdf2cbd88a92b1cf62c251ad7a26377a5b
mwestphal <mathieu.westphal@gmail.com>
2024-06-02 10:22:32 +0000

    Deploying to gh-pages from @ f3d-app/f3d@2aa9d398a739ffc3beb6d998186e4c94234e0efb 🚀

--- web/f3d.js
+++ web/f3d.js
@@ -62,7 +62,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpg4ejvkqz.js
+// include: /tmp/tmpydhcpqqp.js
 
   if (!Module.expectedDataFileDownloads) {
     Module.expectedDataFileDownloads = 0;
@@ -242,21 +242,21 @@ var REMOTE_PACKAGE_SIZE = metadata['remote_package_size'];
 
   })();
 
-// end include: /tmp/tmpg4ejvkqz.js
-// include: /tmp/tmpfd95gdmg.js
+// end include: /tmp/tmpydhcpqqp.js
+// include: /tmp/tmpr_qg5l4i.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpfd95gdmg.js
-// include: /tmp/tmpl3c1gkwn.js
+  // end include: /tmp/tmpr_qg5l4i.js
+// include: /tmp/tmp50jrkktj.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach(function(task) {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpl3c1gkwn.js
+  // end include: /tmp/tmp50jrkktj.js
 
 
 // Sometimes an existing Module object exists with properties
